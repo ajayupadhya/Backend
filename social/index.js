79b87b246a75b7ab96ee19afc91ed571dcec623f
Ajay Upadhyay <ajay.bhumca18@gmail.com>
2021-11-25 12:51:24 +0530

    update

--- social/index.js
+++ social/index.js
@@ -1,11 +1,25 @@
 const express = require("express");
-
+const dateF = require("date-format");
 const app = express();
 
 const PORT = 4000 || process.env.PORT;
 
 app.get("/", (req, res) => {
   res.status(400).send("hello World");
+});
+
+app.get("/api/v1/instagram", (req, res) => {
+  const data = {
+    user: "Ajay Upadhyay",
+    follow: 20,
+    follower: 1000,
+    date: dateF("hh:mm:ss.SSS", new Date()),
+  };
+  res.status(400).json(data);
+});
+
+app.get("/api/v1/:id/", (req, res) => {
+  res.status(200).send(`${req.params.id}`);
 });
 
 app.listen(PORT, () => {
